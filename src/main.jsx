b39f7c0e1b761b589c9dc1f01296cb3f343b320d
shailendrapawar <shailendrapawar980@gmail.com>
2024-11-17 17:12:29 +0530

    ADDED: all pages for minimum requiremnet

--- src/main.jsx
+++ src/main.jsx
@@ -5,21 +5,39 @@ import './index.css'
 import { BrowserRouter, createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom"
 import AuthLayout from './layouts/AuthLayout'
 import Landing from './Landing'
+import RootLayout from './layouts/RootLayout'
+import UserLayouts from './layouts/UserLayouts'
+import Register from './pages/authPages/Register'
+import Login from './pages/authPages/Login'
+import Home from './pages/userPages/homePage/Home'
+import MyRequest from './pages/userPages/myRequestPage/MyRequest'
+import PlanTrip from './pages/userPages/planTripPage/PlanTrip'
+import Explore from './pages/userPages/explorePage/Explore'
+import UserProfile from './pages/userPages/userProfilePage/UserProfile'
+import Post from './pages/userPages/postPage/Post'
 
 
 const myRouter = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<AuthLayout />}>
+    <Route path='/' element={<RootLayout/>}>
+
+      {/* landing page  ===============*/}
       <Route path='' element={<Landing />}></Route>
 
-      <Route path='auth'>
-        <Route path='/register'></Route>
-        <Route path='/login'></Route>
+      {/* auth pages for login and register============ */}
+      <Route path='auth' element={<AuthLayout/>}>
+        <Route path='/auth/register' element={<Register/>}></Route>
+        <Route path='/auth/login' element={<Login/>}></Route>
       </Route>
 
-      <Route path='user'>
-      <Route path='/profile'></Route>
-      <Route path='/'></Route>
+      {/* pages for users=============================== */}
+      <Route path='user' element={<UserLayouts/>}>
+        <Route path='/user/home' element={<Home/>}></Route>
+        <Route path='/user/planTrip' element={<PlanTrip/>}></Route>
+        <Route path='/user/explore' element={<Explore/>}></Route>
+        <Route path='/user/myRequests' element={<MyRequest/>}></Route>
+        <Route path='/user/post' element={<Post/>}></Route>
+        <Route path='/user/profile' element={<UserProfile/>}></Route>
       </Route>
     </Route>
   )
---
+++ src/pages/userPages/planTripPage/PlanTrip.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PlanTrip = () => {
+  return (
+    <div>PlanTrip</div>
+  )
+}
+
+export default PlanTrip
